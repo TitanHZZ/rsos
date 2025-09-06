bdb00155fd858411fc868886803d0cc34cea303c
TitanHZZ <titanhzz2018@gmail.com>
2025-09-06 15:00:30 +0100

    More page allocator work.

--- src/memory/pages/simple_page_allocator.rs
+++ src/memory/pages/simple_page_allocator.rs
@@ -1,4 +1,4 @@
-use crate::{assert_called_once, data_structures::bitmap_ref_mut::BitmapRefMut, kernel::{Kernel, KERNEL}, memory::{frames::{Frame, FrameAllocator}, pages::{page_table::page_table_entry::EntryFlags, Page, PageAllocator}, AddrOps, MemoryError, VirtualAddress, FRAME_PAGE_SIZE, MEMORY_SUBSYSTEM}, serial_println};
+use crate::{assert_called_once, data_structures::bitmap_ref_mut::BitmapRefMut, kernel::{Kernel, KERNEL}, memory::{frames::FrameAllocator, pages::{page_table::page_table_entry::EntryFlags, Page, PageAllocator}, AddrOps, MemoryError, VirtualAddress, FRAME_PAGE_SIZE, MEMORY_SUBSYSTEM}, serial_println};
 use spin::Mutex;
 
 // This page allocator manages the entire higher half of the 48 bit address space, 2 ** 48 // 2 bytes.
@@ -26,36 +26,44 @@ struct BitmapPageAllocatorInner<'a> {
 }
 
 impl<'a> BitmapPageAllocatorInner<'a> {
+    fn page_idx_to_bit_idxs(&self, page_idx: usize) -> (usize, usize) {
+        assert!(page_idx < (Kernel::hh_end() / FRAME_PAGE_SIZE));
+        (page_idx / BitmapPageAllocator::level2_bitmap_bit_lenght(), page_idx % BitmapPageAllocator::level2_bitmap_bit_lenght())
+    }
+
     fn addr_to_bit_idxs(&self, addr: VirtualAddress) -> (usize, usize) {
         assert!(addr >= Kernel::k_hh_start() && addr <= Kernel::hh_end());
-        let page_idx = (addr - Kernel::k_hh_start()).align_down(FRAME_PAGE_SIZE) / FRAME_PAGE_SIZE;
-
-        (page_idx / BitmapPageAllocator::level2_bitmap_bit_lenght(), page_idx % BitmapPageAllocator::level2_bitmap_bit_lenght())
+        let page_idx = (addr.align_down(FRAME_PAGE_SIZE) - Kernel::k_hh_start()) / FRAME_PAGE_SIZE;
+        self.page_idx_to_bit_idxs(page_idx)
     }
 
     fn allocate_level2_bitmap(&mut self, bitmap_idx: usize) -> Result<(), MemoryError> {
         assert!(bitmap_idx < 261120);
 
+        // allocate and map all the required pages for the second level bitmap
         let bitmap_start_addr = BitmapPageAllocator::level2_bitmaps_start_addr() + (BitmapPageAllocator::level2_bitmap_lenght() * bitmap_idx);
         for addr in (bitmap_start_addr..bitmap_start_addr + BitmapPageAllocator::level2_bitmap_lenght()).step_by(FRAME_PAGE_SIZE) {
             MEMORY_SUBSYSTEM.active_paging_context().map(addr, EntryFlags::PRESENT | EntryFlags::WRITABLE | EntryFlags::NO_EXECUTE)?;
         }
 
+        // build the level 2 bitmap
         self.l1[bitmap_idx] = Some(unsafe {
             BitmapRefMut::from_raw_parts_mut(bitmap_start_addr as _, BitmapPageAllocator::level2_bitmap_lenght(), None, true)
         });
 
-        // TODO: the following code must be repeated for all pages belonging to the bitmap
-
+        // recursively allocate the second level bitmap that marks the current one as allocated
         let (l1_idx, l2_idx) = self.addr_to_bit_idxs(bitmap_start_addr);
-        serial_println!("l1: {}; l2: {}", l1_idx, l2_idx);
+        serial_println!("bitmap start addr: {:#x}, index: {}", bitmap_start_addr, bitmap_idx);
+        serial_println!("l1 index: {}; l2 index: {}", l1_idx, l2_idx);
         if self.l1[l1_idx].is_none() {
-            // TODO: allocate this bitmap
-            todo!()
+            self.allocate_level2_bitmap(l1_idx)?;
         }
 
-        // mark the allocator page as allocated in the allocator itself
-        self.l1[l1_idx].as_mut().unwrap().set(l2_idx, true);
+        // mark the current second level bitmap pages as allocated in the new, recursively allocated, second level bitmap
+        for offset in 0..(BitmapPageAllocator::level2_bitmap_lenght() / FRAME_PAGE_SIZE) {
+            self.l1[l1_idx].as_mut().unwrap().set(l2_idx + offset, true);
+        }
+
         Ok(())
     }
 }
@@ -91,10 +99,10 @@ impl<'a> BitmapPageAllocator<'a> {
 
     /// Get the address where the first level 2 bitmap will start.
     fn level2_bitmaps_start_addr() -> VirtualAddress {
-        KERNEL.fa_hh_start() + match MEMORY_SUBSYSTEM.frame_allocator().metadata_memory_range() {
+        (KERNEL.fa_hh_start() + match MEMORY_SUBSYSTEM.frame_allocator().metadata_memory_range() {
             Some(metadata) => metadata.length(),
             None => 0,
-        }
+        }).align_up(Self::level2_bitmap_lenght())
     }
 }
 
@@ -113,11 +121,20 @@ unsafe impl<'a> PageAllocator for BitmapPageAllocator<'a> {
         let level2_bitmap_count = allocated_size_in_pages.align_up(BitmapPageAllocator::level2_bitmap_bit_lenght())
             / BitmapPageAllocator::level2_bitmap_bit_lenght();
 
+        // TODO: this can be improved for performance
+        for page_idx in 0..allocated_size_in_pages {
+            let (l1_idx, l2_idx) = allocator.page_idx_to_bit_idxs(page_idx);
+
+            if allocator.l1[l1_idx].is_none() {
+                allocator.allocate_level2_bitmap(l1_idx)?;
+            }
+
+            allocator.l1[l1_idx].as_mut().unwrap().set(l2_idx, true);
+        }
+
         serial_println!("allocated size: {:#x}", allocated_size);
+        serial_println!("allocated size in pages: {:#x}", allocated_size_in_pages);
         serial_println!("level2 bitmap count: {}", level2_bitmap_count);
-
-        allocator.allocate_level2_bitmap(0)?;
-
         Ok(())
     }
 
